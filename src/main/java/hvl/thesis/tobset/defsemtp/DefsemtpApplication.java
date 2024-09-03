68061807873c659a532862d5e439d09cf9f8fc02
Tobias Setnes <tobset@netcompany.com>
2024-09-03 14:40:07 +0200

    Code rebase

---
+++ src/main/java/hvl/thesis/tobset/defsemtp/DefsemtpApplication.java
@@ -0,0 +1,13 @@
+package hvl.thesis.tobset.defsemtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefsemtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefsemtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hvl/thesis/tobset/defsemtp/DefsemtpApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.thesis.tobset.defsemtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefsemtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
