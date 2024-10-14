f7dc5a10a92ad03ec3fe0892737c0c8836cca951
Tobias Setnes <tobset@netcompany.com>
2024-10-14 17:06:48 +0200

    fix(BpmnToAlloyService): Added logic for handling multiple tokens on the model (e.g. parallel gateways).

--- src/main/java/hvl/thesis/tobset/defsemtp/services/BpmnToAlloyService.java
+++ src/main/java/hvl/thesis/tobset/defsemtp/services/BpmnToAlloyService.java
@@ -3,6 +3,9 @@ package hvl.thesis.tobset.defsemtp.services;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.*;
 import org.springframework.stereotype.Service;
+import org.camunda.bpm.model.bpmn.instance.EndEvent;
+import org.camunda.bpm.model.bpmn.instance.TerminateEventDefinition;
+import java.util.List;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -18,65 +21,119 @@ import java.util.stream.Collectors;
 public class BpmnToAlloyService {
 
     public String generateInitPredicate(BpmnModelInstance modelInstance) {
-        StringBuilder init = new StringBuilder("pred init [] {\n");
+        StringBuilder alloyModel = new StringBuilder();
 
-        // Define element counts
-        init.append("  #Process = 1\n");
-        int startEventCount = modelInstance.getModelElementsByType(StartEvent.class).size();
-        int endEventCount = modelInstance.getModelElementsByType(EndEvent.class).size();
-        int taskCount = modelInstance.getModelElementsByType(Task.class).size();
-        int subprocessCount = modelInstance.getModelElementsByType(SubProcess.class).size();
-        int sequenceFlowCount = modelInstance.getModelElementsByType(SequenceFlow.class).size();
+        int startEventIndex = 1, taskIndex = 1, exclusiveGatewayIndex = 1;
+        int parallelGatewayOpeningIndex = 1, parallelGatewayClosingIndex = 1;
+        int eventBasedGatewayIndex = 1, intermediateCatchEventIndex = 1;
+        int intermediateThrowEventIndex = 1, terminateEndEventIndex = 1;
+        int endEventIndex = 1, subProcessIndex = 1;
 
-        // Set the exact counts for each element type
-        init.append("  #StartEvent = ").append(startEventCount).append("\n");
-        init.append("  #EndEvent = ").append(endEventCount).append("\n");
-        init.append("  #SubProcess = ").append(subprocessCount).append("\n");
-        init.append("  #Task = ").append(taskCount).append("\n");
-        init.append("  #SequenceFlow = ").append(sequenceFlowCount).append("\n");
-        init.append("  #Token = 1\n");
-        init.append("  #ProcessSnapshot = 1\n");
+        for (StartEvent startEvent : modelInstance.getModelElementsByType(StartEvent.class)) {
+            alloyModel.append("one sig startEvent").append(startEventIndex).append(" extends StartEvent {}\n");
+            startEventIndex++;
+        }
+        for (Task task : modelInstance.getModelElementsByType(Task.class)) {
+            alloyModel.append("one sig task").append(taskIndex).append(" extends Task {}\n");
+            taskIndex++;
+        }
+        for (ExclusiveGateway gateway : modelInstance.getModelElementsByType(ExclusiveGateway.class)) {
+            alloyModel.append("one sig exclusiveGateway").append(exclusiveGatewayIndex).append(" extends ExclusiveGateway {}\n");
+            exclusiveGatewayIndex++;
+        }
 
-        init.append("\n  some pSnapshot: ProcessSnapshot, s: StartEvent, ");
-        init.append("sp: SubProcess, subStart: StartEvent, subEnd: EndEvent, mainEnd: EndEvent, act: Task {\n");
+        int parallelGatewayTokenAdjustments = 0;
 
-        // Process StartEvent and EndEvent connections
-        for (SequenceFlow flow : modelInstance.getModelElementsByType(SequenceFlow.class)) {
-            String source = flow.getSource().getId();
-            String target = flow.getTarget().getId();
-
-            if (flow.getSource() instanceof StartEvent) {
-                init.append("    #s.incomingSequenceFlows = 0\n");
-                init.append("    #s.outgoingSequenceFlows = 1\n");
-                init.append("    s.outgoingSequenceFlows.target = ").append(target).append("\n\n");
-            } else if (flow.getTarget() instanceof EndEvent) {
-                init.append("    #").append(target).append(".incomingSequenceFlows = 1\n");
-                init.append("    ").append(source).append(".outgoingSequenceFlows.target = ").append(target).append("\n\n");
+        for (ParallelGateway gateway : modelInstance.getModelElementsByType(ParallelGateway.class)) {
+            if (isParallelGatewayOpening(gateway)) {
+                alloyModel.append("one sig parallelGatewayOpening").append(parallelGatewayOpeningIndex).append(" extends ParallelGateway {}\n");
+                parallelGatewayOpeningIndex++;
+                parallelGatewayTokenAdjustments++;
+            } else {
+                alloyModel.append("one sig parallelGatewayClosing").append(parallelGatewayClosingIndex).append(" extends ParallelGateway {}\n");
+                parallelGatewayClosingIndex++;
+                parallelGatewayTokenAdjustments--;
             }
         }
 
-        // SubProcess setup
-        init.append("    sp.subStartEvent = subStart\n");
-        init.append("    sp.subEndEvents = subEnd\n\n");
-
-        // Task and SequenceFlow mappings
-        for (Task task : modelInstance.getModelElementsByType(Task.class)) {
-            init.append("    #").append(task.getId()).append(".incomingSequenceFlows = 1\n");
-            init.append("    #").append(task.getId()).append(".outgoingSequenceFlows = 1\n");
+        for (EventBasedGateway gateway : modelInstance.getModelElementsByType(EventBasedGateway.class)) {
+            alloyModel.append("one sig eventBasedGateway").append(eventBasedGatewayIndex).append(" extends EventBasedGateway {}\n");
+            eventBasedGatewayIndex++;
         }
 
+        for (IntermediateCatchEvent catchEvent : modelInstance.getModelElementsByType(IntermediateCatchEvent.class)) {
+            alloyModel.append("one sig intermediateCatchEvent").append(intermediateCatchEventIndex).append(" extends IntermediateCatchEvent {}\n");
+            intermediateCatchEventIndex++;
+        }
+
+        for (IntermediateThrowEvent throwEvent : modelInstance.getModelElementsByType(IntermediateThrowEvent.class)) {
+            alloyModel.append("one sig intermediateThrowEvent").append(intermediateThrowEventIndex).append(" extends IntermediateThrowEvent {}\n");
+            intermediateThrowEventIndex++;
+        }
+
+        for (EndEvent endEvent : modelInstance.getModelElementsByType(EndEvent.class)) {
+            if (isTerminateEndEvent(endEvent)) {
+                alloyModel.append("one sig terminateEndEvent").append(terminateEndEventIndex).append(" extends TerminateEndEvent {}\n");
+                terminateEndEventIndex++;
+            } else {
+                alloyModel.append("one sig endEvent").append(endEventIndex).append(" extends EndEvent {}\n");
+                endEventIndex++;
+            }
+        }
+
+        for (SubProcess subProcess : modelInstance.getModelElementsByType(SubProcess.class)) {
+            alloyModel.append("one sig subProcess").append(subProcessIndex).append(" extends SubProcess {}\n");
+            subProcessIndex++;
+        }
+
+        alloyModel.append("\npred init {\n");
+
+        int flowIndex = 1;
         for (SequenceFlow flow : modelInstance.getModelElementsByType(SequenceFlow.class)) {
-            init.append("    ").append(flow.getId()).append(".source = ").append(flow.getSource().getId())
-                    .append(" and ").append(flow.getId()).append(".target = ").append(flow.getTarget().getId()).append("\n");
+            String sourceId = flow.getSource().getId();
+            String targetId = flow.getTarget().getId();
+
+            alloyModel.append("    sequenceFlow").append(flowIndex).append(".source = ").append(sourceId)
+                    .append(" and sequenceFlow").append(flowIndex).append(".target = ").append(targetId).append("\n");
+            flowIndex++;
         }
 
-        // Token Initialization
-        init.append("\n    one t: pSnapshot.tokens {\n");
-        init.append("      t.pos = s\n");
-        init.append("    }\n");
+        startEventIndex = 1;
+        for (StartEvent startEvent : modelInstance.getModelElementsByType(StartEvent.class)) {
+            alloyModel.append("    one t").append(startEventIndex).append(": Token | t").append(startEventIndex)
+                    .append(".pos = startEvent").append(startEventIndex).append("\n");
+            startEventIndex++;
+        }
 
-        init.append("  }\n");
-        init.append("}");
-        return init.toString();
+        alloyModel.append("}\n\n");
+
+        int initialTokens = startEventIndex - 1;
+        int tokenScope = initialTokens + parallelGatewayTokenAdjustments;
+
+        alloyModel.append("run init for ")
+                .append(startEventIndex - 1).append(" StartEvent, ")
+                .append(taskIndex - 1).append(" Task, ")
+                .append(exclusiveGatewayIndex - 1).append(" ExclusiveGateway, ")
+                .append(parallelGatewayOpeningIndex + parallelGatewayClosingIndex - 2).append(" ParallelGateway, ")
+                .append(eventBasedGatewayIndex - 1).append(" EventBasedGateway, ")
+                .append(intermediateCatchEventIndex - 1).append(" IntermediateCatchEvent, ")
+                .append(intermediateThrowEventIndex - 1).append(" IntermediateThrowEvent, ")
+                .append(endEventIndex - 1).append(" EndEvent, ")
+                .append(terminateEndEventIndex - 1).append(" TerminateEndEvent, ")
+                .append(subProcessIndex - 1).append(" SubProcess, ")
+                .append(flowIndex - 1).append(" SequenceFlow, ")
+                .append("1 Process, ").append(tokenScope).append(" Token, 1 ProcessSnapshot");
+
+        return alloyModel.toString();
+    }
+
+    private boolean isParallelGatewayOpening(ParallelGateway gateway) {
+        // if opening --> outgoing flows will be >1
+        return gateway.getOutgoing().size() > 1;
+    }
+
+    public boolean isTerminateEndEvent(EndEvent endEvent) {
+        Collection<TerminateEventDefinition> terminateEventDefinitions = endEvent.getChildElementsByType(TerminateEventDefinition.class);
+        return !terminateEventDefinitions.isEmpty();
     }
 }
